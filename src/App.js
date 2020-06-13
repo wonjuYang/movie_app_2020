9aea67c046714fb15ff2a8502704ba81638a2022
triplej <his1monologue@gmail.com>
2020-06-13 22:26:20 +0900

    20.06.13 오후

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
 
 
@@ -9,18 +12,47 @@ class App extends React.Component{
     movies: [],
   }
 
+  getMovies = async () => {
+    const {
+      data : {
+        data: {movies},
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    this.setState({ movies, isLoading: false})
+  }
+
   componentDidMount(){
-    // 영화 데이터 로딩!
-    setTimeout( () => {
-      this.setState({ isLoading: false});
-    }, 6000);
+    this.getMovies();
   }
 
 
   render(){
-    const { isLoading } = this.state;
-    return <div> {isLoading ? 'Loading ... ' : 'We are ready' } </div>;
+    const { isLoading, movies } = this.state;
+    return( 
+      <section className="container">
+        {isLoading ? (
+          <div className = "loader">
+            <span className="loader__text">'Loading ... ' </span>
+          </div>
+          ) : (
+            <div className="movies">
+            {movies.map(movie => ( 
+               <Movie
+                  key = {movie.id}
+                  id = {movie.id}
+                  year = {movie.year}
+                  title = {movie.title}
+                  summary = {movie.summary}
+                  poster ={movie.medium_cover_image}
+                  genres = {movie.genres}
+              />
+            ))}
+            </div> 
+        )} 
+      </section>
+    );
   }
+
 
 
 
