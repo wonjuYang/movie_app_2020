14c21520895f9b99e5346e60e070b372bdeae140
triplej <his1monologue@gmail.com>
2020-06-15 16:55:36 +0900

    20.06.06 오후

---
+++ src/routes/Detail.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+class Detail extends React.Component {
+    componentDidMount(){
+        const { location, history } = this.props;
+        if( location.state === undefined ){
+            history.push('/');
+        }
+    }
+
+    render() {
+        const { location } = this.props;
+        if(location.state){
+            return <span>{location.state.title}</span>;
+        }else{
+            return null;
+        }
+        
+    }
+    
+}
+
+
+export default Detail;
---
+++ src/routes/Home.js
@@ -0,0 +1,177 @@
+import React from 'react';
+import axios from 'axios';
+import Movie from '../components/Movie';
+import './Home.css';
+
+
+
+class Home extends React.Component{
+
+  state = {
+    isLoading: true,
+    movies: [],
+  }
+
+  getMovies = async () => {
+    const {
+      data : {
+        data: {movies},
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    this.setState({ movies, isLoading: false})
+  }
+
+  componentDidMount(){
+    this.getMovies();
+  }
+
+
+  render(){
+    const { isLoading, movies } = this.state;
+    return( 
+      <section className="container">
+        {isLoading ? (
+          <div className = "loader">
+            <span className="loader__text">'Loading ... ' </span>
+          </div>
+          ) : (
+            <div className="movies">
+            {movies.map(movie => ( 
+               <Movie
+                  key = {movie.id}
+                  id = {movie.id}
+                  year = {movie.year}
+                  title = {movie.title}
+                  summary = {movie.summary}
+                  poster ={movie.medium_cover_image}
+                  genres = {movie.genres}
+              />
+            ))}
+            </div> 
+        )} 
+      </section>
+    );
+  }
+
+
+
+
+//----------------------------------------------------------
+
+
+//   componentDidMount(){
+//     console.log('component rendered');
+//   }
+
+//   componentDidUpdate(){
+//     console.log('i just updated')
+//   }
+
+//   componentWillUnmount(){
+//     console.log('goodbye, world');
+//   }
+
+//   constructor(props){
+//     super(props);
+//     console.log('hello')
+//   }
+
+
+//   state = {
+//     count: 0,
+//   }
+
+//   add = () => {
+//     this.setState(current => ({
+//       count: current.count + 1,
+//     }));
+//   }
+
+//   minus = () => {
+//     this.setState(current => ({
+//       count: current.count - 1,
+//     }));
+//   }
+
+
+//   render(){
+//     console.log('rendering');
+//     return (
+//       <div>
+//         <h1> The number is: {this.state.count}</h1>
+//         <button onClick={this.add}>Add</button>
+//         <button onClick={this.minus}>Minus</button>
+//       </div>
+//     )
+
+//   }
+
+
+//-------------------------------------------------------
+
+
+
+// import PropTypes from 'prop-types';
+
+// function Food({name, picture, rating}){
+//   return(
+//     <div>
+//       <h2>I like { name }</h2>
+//       <h4>{rating}/10.0</h4>
+//       <img src={picture} alt={name} />
+//     </div>
+//   );
+
+// }
+
+// const foodILike = [
+//   {
+//     id: 1,
+//     name: 'hyojin',
+//     image:'http://on7off.com/4thmini/img/gallery/gallery_hyojin.png',
+//     rating: 5,
+//   },
+//   {
+//     id: 2,
+//     name: 'J-us',
+//     image: 'http://on7off.com/4thmini/img/gallery/gallery_jus.png',
+//     rating: 6,
+//   },
+//   {
+//     id: 3,
+//     name: 'etion',
+//     image: 'http://on7off.com/4thmini/img/gallery/gallery_etion.png',
+//     rating: 7,
+//   },
+//   {
+//     id: 4,
+//     name: 'jaeyoung',
+//     image: 'http://on7off.com/4thmini/img/gallery/gallery_wyatt.png',
+//     rating: 8,
+//   },
+//   {
+//     id: 5,
+//     name: 'mingyun',
+//     image: 'http://on7off.com/4thmini/img/gallery/gallery_mk.png',
+//     rating: 5,
+//   },
+// ];
+
+
+// function App() {
+//   return(
+//     <div>
+//       {foodILike.map(dish => (<Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
+//       ))}
+//     </div>
+
+//   );
+// }
+
+// Food.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   picture: PropTypes.string.isRequired,
+//   rating: PropTypes.number,
+// };
+}
+export default Home;
